08a91b6e507d1a35011cf34077e9ff44f2389782
juancho <juan.arciniegasm@upb.edu.co>
2022-02-18 09:08:05 -0500

    editamos la vaina

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void funcPrint(char *pstring);
+int main (void)
+{
+    funcPrint("el diseño gráfico es mi pasión")
+    return(0);
+}
+
+/**
+\brief funcPrint
+\param pstring
+\this variable receives the direction of the string to be printed.
+*/
+
+void funcPrint(char *pstring);
+{
+    printf("%s/n", pstring);
+}
